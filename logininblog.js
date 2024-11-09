9c3fc462be407a1639c8814f4ed668f30c3bcfcc
Caio Kotan <caiokotan@gmail.com>
2024-11-08 23:32:31 -0300

    Mensagem descritiva das alterações

---
+++ logininblog.js
@@ -0,0 +1,73 @@
+function onc(){
+    let nameinp=document.getElementById("nameinp").value;
+    let passinp=document.getElementById("passinp").value;
+
+    let aviso=document.getElementById("aviso");
+
+    if(nameinp==""&&passinp!=""){
+        aviso.innerHTML="insira seu nome";
+    }else if(passinp==""&&nameinp!=""){
+        aviso.innerHTML="insira sua senha";
+    }else if(passinp==""&&nameinp==""){
+        aviso.innerHTML="insira seu nome e senha";
+    }else{
+        aviso.innerHTML="";
+        console.log(window.nv);
+        console.log(window.nv.length);
+        let objnameall=[];
+        let objpassall=[];
+
+        let autname="";
+        let autpass="";
+
+        let autindex=0;
+
+        for(let i in window.nv){
+            let objnames=(Object.values(window.nv[i]))[0]
+            let objpass=(Object.values(window.nv[i]))[1]
+            objnameall.push(objnames);
+            objpassall.push(objpass);
+            if(nameinp==objnames){
+                autname=objnames;
+                autpass=objpass;
+                autindex=i;
+            }
+        }   
+        if(nameinp==autname&&passinp==autpass){
+            aviso.innerHTML="LOGADO"
+            
+        }
+
+    }
+}
+function onc2(){
+    let nameinp2=document.getElementById("nameinp2").value;
+    let passinp=document.getElementById("passinp2").value;
+    let cpassinp=document.getElementById("cpassinp").value;
+    let aviso=document.getElementById("aviso2");
+    let objnameall=[];
+    for(let i in window.nv){
+            let objnames=(Object.values(window.nv[i]))[0]
+            objnameall.push(objnames);
+    }
+    let backerror=0;
+    console.log(objnameall);
+    for(let i in objnameall){
+        if(nameinp2==objnameall[i]){
+            backerror=1;
+        }
+    }
+    if(backerror==1){
+        aviso.innerHTML="nome ja existe";
+    }else{
+        aviso.innerHTML="";
+    }
+    if(backerror!=1){
+        if(passinp!=""&&nameinp2!=""){
+            if(passinp==cpassinp){
+                aviso.innerHTML="CADASTRO REALIZADO";
+                window.actcadastro(nameinp2,passinp);
+            }
+        }
+    }
+}
